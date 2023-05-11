7983a0074f44eb0149b72c56e9e3d2cb888dd787
CebotariTigran <cebotaritigran@gmail.com>
2023-05-11 05:53:50 -0400

    Add all tests for ship object

--- src/ship.js
+++ src/ship.js
@@ -1,7 +1,20 @@
 let Ship = (l) => {
     let shipLength = l
+    let hit = 0
     console.log(shipLength)
-    return {shipLength}
+    let isSunk = () => {
+        if (hit == shipLength) {
+            return true
+        } else {
+            return false
+        }
+    }
+    let hitNumber = () => {
+        hit++;
+        console.log(hit)
+        return hit;
+    }
+    return { shipLength, hitNumber, isSunk }
 }
 
-export {Ship}
+export { Ship }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -4,3 +4,25 @@ test('has length', () => {
     let ship1 = Ship(4);
     expect(ship1.shipLength).toBe(4);
 });
+
+test('ship hit', () => {
+    let ship1 = Ship(4);
+    expect(ship1.hitNumber()).toBe(1);
+});
+
+test('ship sunk', () => {
+    let ship1 = Ship(4);
+    ship1.hitNumber();
+    ship1.hitNumber();
+    ship1.hitNumber();
+    ship1.hitNumber();
+    expect(ship1.isSunk()).toBe(true);
+});
+
+test('ship is not sunk', () => {
+    let ship1 = Ship(4);
+    ship1.hitNumber();
+    ship1.hitNumber();
+    ship1.hitNumber();
+    expect(ship1.isSunk()).toBe(false);
+});
