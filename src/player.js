aa549ad6a5bbdf6db60354319a00858430c3034b
CebotariTigran <cebotaritigran@gmail.com>
2023-07-10 12:55:15 -0400

    Update player

--- src/player.js
+++ src/player.js
@@ -1,7 +1,14 @@
+import { GameBoard } from "./gameboard";
+import { Ship } from "./ship";
+
 // doesn't have ai yet this is just player and his turn and 
 let Player = (playerName) => {
     const playerName = playerName;
     const turn = false;
+    const previousAttacks = [];
+    let gameboard = GameBoard(4);
+    let newShip = Ship(2, [0, 8])
+    gameboard.placeShip(newShip.shipPosition)
 
     // to check the turn, might be very useless funtion
     let checkTurn = () => {
@@ -17,10 +24,16 @@ let Player = (playerName) => {
         }
     }
 
+    let attack = (coordinate) => {
+        previousAttacks.push(coordinate);
+        gameboard.receiveAttack(coordinate);
+    }
+
     return {
         turn,
         checkTurn,
-        setTurn
+        setTurn,
+        attack
     }
 }
 
