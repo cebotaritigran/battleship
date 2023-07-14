35cee5e23cc2c263699e37b434b02ac482df69ec
CebotariTigran <cebotaritigran@gmail.com>
2023-07-14 12:34:31 -0400

    Add more test for player

--- src/player.js
+++ src/player.js
@@ -4,16 +4,17 @@ import { Ship } from "./ship";
 // doesn't have ai yet this is just player and his turn and 
 let Player = (playerName) => {
     const playerN = playerName;
-    const turn = false;
+    let turn = false;
+    let enemyTurn = true;
     // to store previous attacks and not let them be repeated
     const previousAttacks = [];
-    let gameboard = GameBoard(4);
+    let gameboard = GameBoard([1, 9]);
     // let newShip = Ship(2, [0, 8])
     // gameboard.placeShip(newShip.shipPosition)
 
     // to check the turn, might be very useless funtion
     let checkTurn = () => {
-        return this.turn;
+        return turn;
     }
 
     // set turn to false when player has played his turn 
@@ -28,6 +29,7 @@ let Player = (playerName) => {
     let attack = (coordinate) => {
         previousAttacks.push(coordinate);
         gameboard.receiveAttack(coordinate);
+        return gamboard.coordinates
     }
 
     return {
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -3,11 +3,115 @@ import { Ship } from "../ship";
 import { Player } from "../player";
 
 test('player 1 has his gameboard', () => {
-    let tigran = Player("tigran");
-    expect(tigran.gameboard.coordinates).toStrictEqual([
+    let tigrits = Player("tigrits");
+    expect(tigrits.gameboard.coordinates).toStrictEqual([
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+});
+
+test('player 1 has his gameboard', () => {
+    let tigrits = Player("tigrits");
+    let ai = Player("ai");
+    expect(ai.gameboard.coordinates).toStrictEqual([
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+});
+
+test('check player1s turn', () => {
+    let tigrits = Player("tigrits");
+    expect(tigrits.checkTurn()).toStrictEqual(false);
+});
+
+test('player1 placeship on the board', () => {
+    let tigrits = Player("tigrits");
+    let ai = Player("ai");
+    let destroyer = Ship(2, [0, 8])
+    expect(tigrits.gameboard.placeShip(destroyer.shipPosition)).toStrictEqual([
+        1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+});
+
+test('player1 and player2 placeship on the board on different spots', () => {
+    let tigrits = Player("tigrits");
+    let destroyer = Ship(2, [0, 8])
+    expect(tigrits.gameboard.placeShip(destroyer.shipPosition)).toStrictEqual([
+        1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+
+    let ai = Player("ai");
+    let destroyerPlayerTwo = Ship(2, [17, 18])
+    expect(ai.gameboard.placeShip(destroyerPlayerTwo.shipPosition)).toStrictEqual([
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+});
+
+test('player1 attacks player2 hits first attack and misses the second', () => {
+    let tigrits = Player("tigrits");
+    let destroyer = Ship(2, [0, 8])
+    expect(tigrits.gameboard.placeShip(destroyer.shipPosition)).toStrictEqual([
+        1, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+
+
+    let ai = Player("ai");
+    let destroyerPlayerTwo = Ship(2, [17, 18])
+    ai.gameboard.placeShip(destroyerPlayerTwo.shipPosition)
+    expect(ai.gameboard.receiveAttack(17)).toStrictEqual([
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 3, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    ]);
+    expect(ai.gameboard.receiveAttack(16)).toStrictEqual([
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        2, 3, 1, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
