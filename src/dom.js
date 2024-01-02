756bdcf9fe7775485e4c337db0bdf39859159ae6
CebotariTigran <cebotaritigran@gmail.com>
2024-01-02 14:24:39 -0500

    make ships appear on the box

--- src/dom.js
+++ src/dom.js
@@ -3,24 +3,34 @@ import { Ship } from "./ship";
 import { Player } from "./player";
 
 function createBoard() {
-    let ships = [0, 10, 20, 30, 5, 15, 9, 29, 35, 45, 49, 69, 65, 75, 61, 62, 63, 81, 82, 83]
+    let ships = [[0, 10, 20, 30], [5, 15], [9, 29], [35, 45], [49, 69], [65, 75], [61, 62, 63], [81, 82, 83]]
+
     const gridContainer = document.getElementsByClassName('gridContainer');
-    for (let k = 0; k < 100; k++) {
-        for (let i = 0; i < 100; i++) {
-            if (k < 1) { // only once prints the grids on container
-                const div = document.createElement('div')
-                div.classList.add('gridBox');
-                div.setAttribute("value", i)
-                gridContainer[0].appendChild(div);
-            }
+
+    for (let i = 0; i < 100; i++) {
+        const div = document.createElement('div')
+        div.classList.add('gridBox');
+        div.setAttribute("value", i)
+        gridContainer[0].appendChild(div);
+    }
+
+    for (let k = 0; k < ships.length; k++) {
+        const gridBox = document.getElementsByClassName('gridBox');
+        
+        for (let i = 0; i < ships[k].length; i++) {
+            gridBox[ships[0][i]].classList.add('ship')
             //prints the ships on the grids (to be changed)
-            const gridBox = document.getElementsByClassName('gridBox');
-            if (ships[k] == gridBox[i].getAttribute("value")) {
-                gridBox[i].classList.add('ship')
-                console.log("working")
-            }
+            
+            // console.log(ships[k][i])
+            // if (ships[k][i] == gridBox[i].getAttribute("value")) {
+            //     console.log(ships[k][i])
+                
+            //     console.log("working")
+            // }
+            gridBox[ships[k][i]].classList.add('shipPart')
         }
     }
+
 
     // const gridBox = document.getElementsByClassName('gridBox');
     // for (let i = 0; i < 100; i++) {
