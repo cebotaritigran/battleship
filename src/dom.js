cd605878b6c3d24fb7dc63112b7f391d4f79f484
CebotariTigran <cebotaritigran@gmail.com>
2024-01-05 12:00:49 -0500

    try to rotate the ship

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,12 @@ import { Ship } from "./ship";
 import { Player } from "./player";
 
 function createBoardPlayerOne() {
-    let ships = [[0, 10, 20, 30], [5, 15], [9, 29], [35, 45], [49, 69], [65, 75], [61, 62, 63], [81, 82, 83]]
+
+    let tigrits = Player("tigrits")
+    let destroyer = Ship(2, [0, 1])
+    console.log(destroyer.shipPosition)
+    let ships = [destroyer.shipPosition, [5, 15], [9, 29], [35, 45], [49, 69], [65, 75], [61, 62, 63], [81, 82, 83]]
+    console.log(ships[0])
 
     const gridContainer = document.getElementsByClassName('gridContainer');
 
@@ -16,19 +21,25 @@ function createBoardPlayerOne() {
 
     for (let k = 0; k < ships.length; k++) {
         const gridBox = document.getElementsByClassName('gridBox');
-        
-        for (let i = 0; i < ships[k].length; i++) {
-            gridBox[ships[k][i]].classList.add('ship')
+
+        for (let i = 0; i < ships[0].length; i++) {
+            gridBox[ships[0][i]].classList.add('ship')
             //prints the ships on the grids (to be changed)
-            
+
             // console.log(ships[k][i])
             // if (ships[k][i] == gridBox[i].getAttribute("value")) {
             //     console.log(ships[k][i])
-                
+
             //     console.log("working")
             // }
-            gridBox[ships[k][i]].classList.add('shipPart')
+            // gridBox[ships[k][i]].classList.add('shipPart')
         }
+    }
+    const ship = document.getElementsByClassName('ship');
+    for (let i = 0; i < ship.length; i++) {
+        ship[i].addEventListener("click", () => {
+            
+        })
     }
 
 
