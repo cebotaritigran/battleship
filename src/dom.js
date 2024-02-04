c759d6935b7ebdcdb8e2003dd7377fb51613f24d
CebotariTigran <cebotaritigran@gmail.com>
2024-02-04 05:57:31 -0500

    clean code

--- src/dom.js
+++ src/dom.js
@@ -1,18 +1,22 @@
 import { GameBoard } from "./gameboard";
 import { Ship } from "./ship";
 import { Player } from "./player";
+
+// rotate is there for checking if the ship is vertical or horizontal
 let rotate = false;
+// creating player
 let tigrits = Player("tigrits")
+// creating ships and putting it in the ships array
 let destroyer = Ship(2, [0, 10, 20, 30])
 console.log(destroyer.shipPosition)
-let ships = [destroyer.shipPosition, [4, 14], [9, 29], [35, 45], [49, 69], [65, 75], [61, 62, 63], [81, 82, 83]]
+let ships = [destroyer.shipPosition, [4, 14], [9, 19], [35, 45], [49, 59], [65, 75], [61, 62, 63], [81, 82, 83]]
 
 console.log(ships[0])
+//getting elements to manipulate
 const gridContainer = document.getElementsByClassName('gridContainer');
-
 const gridBox = document.getElementsByClassName('gridBox');
 
-
+//creating board
 function createBoardPlayerOne() {
 
     for (let i = 0; i < 100; i++) {
@@ -22,19 +26,24 @@ function createBoardPlayerOne() {
         div.setAttribute("id", i)
         gridContainer[0].appendChild(div);
     }
+    // putting ships on the board
     refreshBoard()
 }
 
+// function that adds ships to given tiles based by their coordinates
 function refreshBoard() {
     for (let k = 0; k < ships.length; k++) {
         for (let i = 0; i < ships[k].length; i++) {
             gridBox[ships[k][i]].classList.add('shipPart')
             let shipPart = document.getElementsByClassName('shipPart');
             gridBox[ships[k][i]].addEventListener('click', () => {
+                // whecn clicked we send the given ship to rotate ships function
                 rotateShips(k, i);
+                // we then reset the board because inside rotateShip we change coordinates of the ship and we need to refresh the board
                 gridContainer[0].innerHTML = '';
                 createBoardPlayerOne()
             })
+            // silly stuff
             console.log("hey")
             document.body.style.backgroundColor = "white";
         }
@@ -44,9 +53,14 @@ function refreshBoard() {
 
 function rotateShips(k, i) {
     console.log("hey")
+    // checking each ships length
     if (ships[k].length == 4) {
+        // checking if the ship is on the edge if it is then we do not rotate it
+        // because of the last tile on the row is always 9, 19, 29, I added +1 to them and ask for remainder if it is 0 then we do not rotate
+        // the ship and it just doesn't do anything but if there is a remainder other than 0 then we rotate the ship
         if ((ships[k][i] + 1) % 10 !== 0) {
             if (rotate == false) {
+                // making ship rotate horizontal
                 gridBox[ships[k][0] + 1].classList.add('shipPart');
                 console.log(gridBox[ships[k][0] + 1].getAttribute("value"))
                 console.log(ships[k][1] = gridBox[ships[k][0] + 1].getAttribute("value"))
@@ -66,6 +80,7 @@ function rotateShips(k, i) {
                 rotate = true;
 
             } else {
+                 // making ship rotate verticale
                 gridBox[ships[k][0] + 1].classList.remove('shipPart')
                 gridBox[ships[k][0] + 10].classList.add('shipPart');
                 console.log(gridBox[ships[k][0] + 10].getAttribute("value"))
@@ -87,7 +102,7 @@ function rotateShips(k, i) {
         }
     } else if (ships[k].length == 3) {
         if ((ships[k][i] + 1) % 10 !== 0) {
-
+           
             if (rotate == false) {
                 gridBox[ships[k][0] + 1].classList.add('shipPart')
                 console.log(gridBox[ships[k][0] + 1].getAttribute("value"))
@@ -116,8 +131,9 @@ function rotateShips(k, i) {
             }
         }
     } else if (ships[k].length == 2) {
+        console.log(ships[k][i] + 1 % 10 !== 0) 
         if ((ships[k][i] + 1) % 10 !== 0) {
-
+            console.log(ships[k][i] + 1)
             if (rotate == false) {
                 gridBox[ships[k][0] + 1].classList.add('shipPart')
                 console.log(gridBox[ships[k][0] + 1].getAttribute("value"))
