d63aa8a7c9294103e4f9c0fe1328b775aa357838
CebotariTigran <cebotaritigran@gmail.com>
2024-02-09 14:00:21 -0500

    clean code

--- src/dom.js
+++ src/dom.js
@@ -54,15 +54,19 @@ function refreshBoard() {
 function rotateShips(k, i) {
     console.log("hey")
     // checking each ships length
+    for (let j = 0; j < ships.length; j++) {
+        if(ships[j].includes())
+    }
     if (ships[k].length == 4) {
+        console.log(Number(ships[k][ships[k].length - 1]) + 1)
         // checking if the ship is on the edge if it is then we do not rotate it
         // because of the last tile on the row is always 9, 19, 29, I added +1 to them and ask for remainder if it is 0 then we do not rotate
         // the ship and it just doesn't do anything but if there is a remainder other than 0 then we rotate the ship
         if ((ships[k][i] + 1) % 10 !== 0) {
-            console.log(ships.includes(ships[k][ships[k].length - 1]))
-            let length = Number(ships[k][ships[k].length - 1])
-            console.log(length + 1)
-            console.log(ships[k + 1].includes(Number(length + 1)))
+            // console.log(ships.includes(ships[k][ships[k].length - 1]))
+            // let length = Number(ships[k][ships[k].length - 1])
+            // console.log(length + 1)
+            // console.log(ships[k + 1].includes(Number(length + 1)))
             if (rotate == false) {
                 // making ship rotate horizontal
                 // adding new tiles to the ships array as new ship position
@@ -156,9 +160,7 @@ function rotateShips(k, i) {
             }
         }
     }
-    let length = Number(ships[k][ships[k].length - 1])
-    console.log(length + 1)
-    console.log(ships[k + 1].includes(Number(length + 1)))
+
 }
 
 export { createBoardPlayerOne }
